c5c7d422d223b17bd22d25059ba6d03d862467b0
TaishiMatsuda <61358849+TaishiMatsuda@users.noreply.github.com>
2020-05-16 23:18:07 -0300

    Updated Bubble Chart

--- plots.js
+++ plots.js
@@ -9,15 +9,9 @@ function init() {
           .append("option")
           .text(sample)
           .property("value", sample);
-      });
-
-    top10Bacteria = [{
-      type: 'bar',
-      x: [1, 2, 3, 4, 5],
-      y: ['a','b','c','d','e'],
-      orientation: 'h'
-    }];
-    Plotly.newPlot("bar", top10Bacteria);
+        });
+      buildMetadata(sampleNames[0]);
+      buildCharts(sampleNames[0]);
 })}
   
 init();
@@ -42,9 +36,54 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     var sampledata = data.samples.filter(sampleObj => sampleObj.id == sample);
-    var sorteddata = sampledata.sort((a,b) => b.sample_values - a.sample_values);
-    topTenValues = sorteddata.map(values => values.sample_values);
-    console.log(topTenValues);
+    var result = sampledata[0];
+
+    // Building Top 10 Horizontal Bar Chart
+    var topTenIDs = result.otu_ids.map(id=>`OTU ${id}`).slice(0,10).reverse();
+    var topTenLabels = result.otu_labels.slice(0,10).reverse();
+    var topTenValues = result.sample_values.slice(0,10).reverse();
+
+    var trace1 = {
+      x: topTenValues,
+      y: topTenIDs,
+      type: "bar",
+      orientation: "h",
+      marker: {
+        color: 'rgba(55,128,191,0.6)',
+        width: 1
+      },
+      text: topTenLabels
+    }
+    data1 = [trace1];
+    layout1 = {
+      title: 'Top 10 OTUs',
+      paper_bgcolor: 'rgb(248,248,255)',
+      plot_bgcolor: 'rgb(248,248,255)',
+      margin: {
+        l: 100,
+        r: 20,
+        t: 50,
+        b: 20
+      },
+    };
+    Plotly.newPlot("bar", data1, layout1)
+
+    // Building Bubble Charts
+    var allIDs = result.otu_ids;
+    var allValues = result.sample_values;
+    var markerSize = result.sample_values;
+
+    var trace2 = {
+      x: allIDs,
+      y: allValues,
+      mode: 'markers',
+      marker: {
+        size: markerSize,
+        color: allIDs
+      }
+    }
+    data2 = [trace2];
+    Plotly.newPlot("bubble", data2);
   }
   )
 }
